66b74bbf0f98e204c33272ce3634f2d5cc0fb548
ditramadia <ditrarizqaamadia@gmail.com>
2024-07-03 14:37:06 +0700

    feat: add login page

---
+++ pages/login/index.vue
@@ -0,0 +1,46 @@
+<script setup>
+  const loading = ref(false)
+
+  const user = ref({
+    email: '',
+    password: ''
+  })
+
+  const login = () => {
+    loading.value = true
+    setTimeout(() => {
+      // TODO: Login logic
+      console.log("Login successful")
+      loading.value = false
+    }, 2000);
+  }
+</script>
+
+<template>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <div class="max-w-[400px] w-full">
+      <h1 class="mb-8 text-center text-neutral-100 text-4xl font-[600]">Login</h1>
+      <form @submit.prevent="login" class="flex flex-col gap-2">
+        <input 
+          type="email" 
+          v-model="user.email" 
+          placeholder="Email" 
+          class="text-input"
+          required
+        />
+        <input 
+          type="password" 
+          v-model="user.password" 
+          placeholder="Password" 
+          class="text-input"
+          required
+        />
+        <button 
+          type="submit"
+          :disabled="loading"
+          :class="`button mt-4 ${loading && 'opacity-60'} bg-yellow-400 text-neutral-900 font-[700] transition cursor-pointer hover:bg-yellow-500`"
+        >{{ loading ? 'Logging in...' : 'Login' }}</button>
+      </form>
+    </div>
+  </div>
+</template>
