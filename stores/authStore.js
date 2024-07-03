f690c07eeefc9943e2ca3fb842d08871e9262b9c
ditramadia <ditrarizqaamadia@gmail.com>
2024-07-03 15:57:29 +0700

    feat: add authorization middleware

--- stores/authStore.js
+++ stores/authStore.js
@@ -1,18 +1,24 @@
-export const useAuthStore = defineStore("auth", () => {
-  const token = ref(null);
-  const user = ref({ email: "" });
+export const useAuthStore = defineStore(
+  "auth",
+  () => {
+    const token = ref(null);
+    const user = ref({ email: "" });
 
-  const isAuthenticated = computed(() => token.value !== null);
+    const isAuthenticated = computed(() => token.value !== null);
 
-  function login(email) {
-    token.value = "wDsAWsdAwdjLSdwaKdJDwiSLAKd";
-    user.value.email = email;
+    function login(email) {
+      token.value = "wDsAWsdAwdjLSdwaKdJDwiSLAKd";
+      user.value.email = email;
+    }
+
+    function logout() {
+      token.value = null;
+      user.value.email = "";
+    }
+
+    return { token, user, isAuthenticated, login, logout };
+  },
+  {
+    persist: true,
   }
-
-  function logout() {
-    token.value = null;
-    user.value.email = "";
-  }
-
-  return { token, user, isAuthenticated, login, logout };
-});
+);
